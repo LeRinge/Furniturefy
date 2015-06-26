fc90f8979dbc2a936a43e47087f2a31392374102
Lu1zZz <lu1zzz.dsc@gmail.com>
2015-06-26 00:59:21 -0500

    Shows image from cloudanary

---
+++ config.js
@@ -0,0 +1,10 @@
+var cloudinary = require('cloudinary');
+var config = {}
+
+cloudinary.config({ 
+  cloud_name: 'hfhpkva95', 
+  api_key: '536238958693214', 
+  api_secret: '-TNY5t15FajU_yBdwGKLnRSigGY' 
+});
+
+module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config');
 var cloudinary = require('cloudinary');
 
-cloudinary.uploader.upload("MongoDB.jpg",
-function(result) { console.log(result) })
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express in Heroku!!' });
+    res.render('index', { title: 'Express' , imageURL:'http://res.cloudinary.com/hfhpkva95/image/upload/v1435295611/vucpreq4irbaklkt1k1m.png' });
 });
 
 module.exports = router;
