f676bd5b6ccf58c7efbc60d2084588e318147f23
Lu1zZz <lu1zzz.dsc@gmail.com>
2015-06-25 21:59:21 -0500

    Prod v 0.10

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var cloudinary = require('cloudinary');
 
+cloudinary.uploader.upload("MongoDB.jpg",
+function(result) { console.log(result) })
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express Heroku!!' });
